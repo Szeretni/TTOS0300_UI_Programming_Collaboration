e9e0521898c10dbaabbf3d47c25420c8954a6aa5
L4623 <L4623@student.jamk.fi>
2018-03-27 15:52:51 +0300

    Added grid and autostretch

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace TTOS0300_UI_Programming_Collaboration
 {
@@ -21,14 +22,103 @@ namespace TTOS0300_UI_Programming_Collaboration
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int windowWidth = 0;
+        public static int windowHeight = 0;
         public MainWindow()
         {
             InitializeComponent();
 
+            Color b = Color.FromRgb(0, 0, 0);
+
+            List<string> streets = new List<string>();
+
+            double width = 520;
+            double asdf = 100;
+            for (int i = 0;i<36;i++)
+            {
+                streets.Add(i.ToString());
+            }
+
+            double widthpercent = width/100*10;
+            string widthth = widthpercent.ToString(); ;
+
+            for (int i = 0, j = -65;i< 10;i++, j+=65)
+            {
+                if (i == 0)
+                {
+                    Text(100/2, 50, streets[i], b);
+                }
+                else if (i == 1)
+                {
+                    Text(100 + 65 / 2, 50, streets[i], b);
+                }
+                else if (i == 9)
+                {
+                    Text(720-50, 50, streets[i], b);
+                }
+                else
+                {
+                    Text(100 + (65/2+j), 50, streets[i], b);
+                }
+                
+            }
+
+            for (int i = 0, j = -65; i < 10; i++, j += 65)
+            {
+                if (i == 0)
+                {
+
+                }
+                else if (i == 1)
+                {
+                    Text(50, 100 + 65 / 2, streets[i], b);
+                }
+                else if (i == 9)
+                {
+
+                }
+                else
+                {
+                    Text(50, 100 + (65 / 2 + j), streets[i], b);
+                }
+
+            }
+            /*
+            for (int i = 0, j = 10; i < 36; i++, j += 10)
+            {
+                Text(i + j, i + j, "asdf", b);
+            }
+
+            for (int i = 0, j = 10; i < 36; i++, j += 10)
+            {
+                Text(i + j, i + j, "asdf", b);
+            }
+
+            for (int i = 0, j = 10; i < 36; i++, j += 10)
+            {
+                Text(i + j, i + j, "asdf", b);
+            }
+            */
             //PushEffectExample pee = new PushEffectExample();
         }
 
-        protected override void OnRender(DrawingContext drawingContext)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // get our game window area
+            FrameworkElement client = this.Content as FrameworkElement;
+            windowWidth = (int)client.ActualWidth;
+            windowHeight = (int)client.ActualHeight;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // get our game window area
+            FrameworkElement client = this.Content as FrameworkElement;
+            windowWidth = (int)client.ActualWidth;
+            windowHeight = (int)client.ActualHeight;
+        }
+
+        private void CreateText(DrawingContext drawingContext)
         {
             string testString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor";
 
@@ -39,7 +129,7 @@ namespace TTOS0300_UI_Programming_Collaboration
                 FlowDirection.LeftToRight,
                 new Typeface("Verdana"),
                 32,
-                Brushes.White);
+                Brushes.Black);
 
             // Set a maximum width and height. If the text overflows these values, an ellipsis "..." appears.
             formattedText.MaxTextWidth = 300;
@@ -64,7 +154,28 @@ namespace TTOS0300_UI_Programming_Collaboration
             formattedText.SetFontStyle(FontStyles.Italic, 28, 28);
 
             // Draw the formatted text string to the DrawingContext of the control.
+            //drawingContext.DrawText(formattedText, new Point(0, 0));
+
             drawingContext.DrawText(formattedText, new Point(0, 0));
+
+            drawingContext.Close();
+        }
+
+        private void Text(double x, double y, string text, Color color)
+        {
+
+            TextBlock textBlock = new TextBlock();
+
+            textBlock.Text = text;
+
+            textBlock.Foreground = new SolidColorBrush(color);
+
+            Canvas.SetLeft(textBlock, x);
+
+            Canvas.SetTop(textBlock, y);
+            
+            gridObj.Children.Add(textBlock);
+
         }
     }
 }
