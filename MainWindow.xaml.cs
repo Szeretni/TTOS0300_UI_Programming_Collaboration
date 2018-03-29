86ece2597b0f11c31fedfc1c978effee8813d63c
Hannu Oksman <hannu@muaddibs.net>
2018-03-29 17:04:08 +0300

    print to method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,29 +24,62 @@ namespace TTOS0300_UI_Programming_Collaboration
     {
         public static int windowWidth = 0;
         public static int windowHeight = 0;
+        List<string> streets = new List<string>();
+        Color b = Color.FromRgb(0, 0, 0);
+
         public MainWindow()
         {
             InitializeComponent();
-
-            Color b = Color.FromRgb(0, 0, 0);
-
-            List<string> streets = new List<string>();
-
-            double width = 520;
-            double asdf = 100;
             for (int i = 0;i<36;i++)
             {
                 streets.Add(i.ToString());
             }
+        }
 
-            double widthpercent = width/100*10;
-            string widthth = widthpercent.ToString(); ;
-            
-            for (int i = 0, j = -65;i< 10;i++, j+=65)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // get our game window area
+            FrameworkElement client = this.Content as FrameworkElement;
+            windowWidth = (int)client.ActualWidth;
+            windowHeight = (int)client.ActualHeight;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // get our game window area
+            FrameworkElement client = this.Content as FrameworkElement;
+            windowWidth = (int)client.ActualWidth;
+            windowHeight = (int)client.ActualHeight;
+            canvasObj.Children.Clear();
+            PrintText(streets,b);
+        }
+
+        private void Text(double x, double y, string text, Color color)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = text;
+            textBlock.Foreground = new SolidColorBrush(color);
+            Canvas.SetLeft(textBlock, x);
+            Canvas.SetTop(textBlock, y);
+            canvasObj.Children.Add(textBlock);
+        }
+
+        private void PrintText(List<string> streets, Color b)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Text((windowWidth / 10) * (1 + i), windowHeight / 10, streets[i], b);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Text(windowWidth / 10, (windowHeight / 10) * (1 + i), streets[i], b);
+            }
+            /*
+            for (int i = 0, j = -65; i < 10; i++, j += 65)
             {
                 if (i == 0)
                 {
-                    Text(100/2, 50, streets[i], b);
+                    Text(100 / 2, 50, streets[i], b);
                 }
                 else if (i == 1)
                 {
@@ -54,13 +87,12 @@ namespace TTOS0300_UI_Programming_Collaboration
                 }
                 else if (i == 9)
                 {
-                    Text(720-50, 50, streets[i], b);
+                    Text(720 - 50, 50, streets[i], b);
                 }
                 else
                 {
-                    Text(100 + (65/2+j), 50, streets[i], b);
+                    Text(100 + (65 / 2 + j), 50, streets[i], b);
                 }
-                
             }
 
             for (int i = 0, j = -65; i < 10; i++, j += 65)
@@ -81,58 +113,8 @@ namespace TTOS0300_UI_Programming_Collaboration
                 {
                     Text(50, 100 + (65 / 2 + j), streets[i], b);
                 }
-
-            }
-            /*
-            for (int i = 0, j = 10; i < 36; i++, j += 10)
-            {
-                Text(i + j, i + j, "asdf", b);
-            }
-
-            for (int i = 0, j = 10; i < 36; i++, j += 10)
-            {
-                Text(i + j, i + j, "asdf", b);
-            }
-
-            for (int i = 0, j = 10; i < 36; i++, j += 10)
-            {
-                Text(i + j, i + j, "asdf", b);
             }
             */
-            //PushEffectExample pee = new PushEffectExample();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            // get our game window area
-            FrameworkElement client = this.Content as FrameworkElement;
-            windowWidth = (int)client.ActualWidth;
-            windowHeight = (int)client.ActualHeight;
-        }
-
-        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            // get our game window area
-            FrameworkElement client = this.Content as FrameworkElement;
-            windowWidth = (int)client.ActualWidth;
-            windowHeight = (int)client.ActualHeight;
-        }
-
-        private void Text(double x, double y, string text, Color color)
-        {
-
-            TextBlock textBlock = new TextBlock();
-
-            textBlock.Text = text;
-
-            textBlock.Foreground = new SolidColorBrush(color);
-
-            Canvas.SetLeft(textBlock, x);
-
-            Canvas.SetTop(textBlock, y);
-            
-            canvasObj.Children.Add(textBlock);
-
         }
     }
 }
