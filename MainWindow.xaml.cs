a75af080c8c11fbed916182712c8ad458e59a58a
L4623 <L4623@student.jamk.fi>
2018-03-29 16:23:31 +0300

    Moved back to using canvas

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -41,7 +41,7 @@ namespace TTOS0300_UI_Programming_Collaboration
 
             double widthpercent = width/100*10;
             string widthth = widthpercent.ToString(); ;
-
+            
             for (int i = 0, j = -65;i< 10;i++, j+=65)
             {
                 if (i == 0)
@@ -118,49 +118,6 @@ namespace TTOS0300_UI_Programming_Collaboration
             windowHeight = (int)client.ActualHeight;
         }
 
-        private void CreateText(DrawingContext drawingContext)
-        {
-            string testString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor";
-
-            // Create the initial formatted text string.
-            FormattedText formattedText = new FormattedText(
-                testString,
-                CultureInfo.GetCultureInfo("en-us"),
-                FlowDirection.LeftToRight,
-                new Typeface("Verdana"),
-                32,
-                Brushes.Black);
-
-            // Set a maximum width and height. If the text overflows these values, an ellipsis "..." appears.
-            formattedText.MaxTextWidth = 300;
-            formattedText.MaxTextHeight = 240;
-
-            // Use a larger font size beginning at the first (zero-based) character and continuing for 5 characters.
-            // The font size is calculated in terms of points -- not as device-independent pixels.
-            formattedText.SetFontSize(36 * (96.0 / 72.0), 0, 5);
-
-            // Use a Bold font weight beginning at the 6th character and continuing for 11 characters.
-            formattedText.SetFontWeight(FontWeights.Bold, 6, 11);
-
-            // Use a linear gradient brush beginning at the 6th character and continuing for 11 characters.
-            formattedText.SetForegroundBrush(
-                                    new LinearGradientBrush(
-                                    Colors.Orange,
-                                    Colors.Teal,
-                                    90.0),
-                                    6, 11);
-
-            // Use an Italic font style beginning at the 28th character and continuing for 28 characters.
-            formattedText.SetFontStyle(FontStyles.Italic, 28, 28);
-
-            // Draw the formatted text string to the DrawingContext of the control.
-            //drawingContext.DrawText(formattedText, new Point(0, 0));
-
-            drawingContext.DrawText(formattedText, new Point(0, 0));
-
-            drawingContext.Close();
-        }
-
         private void Text(double x, double y, string text, Color color)
         {
 
@@ -174,7 +131,7 @@ namespace TTOS0300_UI_Programming_Collaboration
 
             Canvas.SetTop(textBlock, y);
             
-            gridObj.Children.Add(textBlock);
+            canvasObj.Children.Add(textBlock);
 
         }
     }
