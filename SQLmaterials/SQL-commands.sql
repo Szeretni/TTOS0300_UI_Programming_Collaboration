e666be57884390ebf1289559936282bbeffb0c88
atarvainen <33045386+atarvainen@users.noreply.github.com>
2018-04-09 21:13:50 +0300

    Update SQL-commands.sql

--- SQLmaterials/SQL-commands.sql
+++ SQLmaterials/SQL-commands.sql
@@ -4,7 +4,7 @@ INNER JOIN Player_has_Cash ON Player_has_Cash.PlayerId = Player.PlayerId
 INNER JOIN Cash ON Cash.CashId = Player_has_Cash.CashId;
 
 -- Query for total player cash in every gamesession
-SELECT PlayerName, GameSessionId, sum(Value) FROM Player 
+SELECT PlayerName, GameSessionId, sum(Value) AS Cash FROM Player 
 INNER JOIN Player_has_Cash ON Player_has_Cash.PlayerId = Player.PlayerId 
 INNER JOIN Cash ON Cash.CashId = Player_has_Cash.CashId 
 GROUP BY PlayerName;
