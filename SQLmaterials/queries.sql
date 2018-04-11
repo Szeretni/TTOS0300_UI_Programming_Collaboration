69ac8cd1f57e335b4263a0b6bdb3ba0b07663f71
Szeretni <35224428+Szeretni@users.noreply.github.com>
2018-04-11 15:40:10 +0300

    on update delimiter

--- SQLmaterials/queries.sql
+++ SQLmaterials/queries.sql
@@ -2,7 +2,7 @@
 -- Some of the material is just for reference ie. how to set isolation levels.
 -- Some is added to schema.sql
 
--- DDL
+-- DDL temp
 alter table Cell modify column Name varchar(256);
 alter table Cell modify column CellId int auto_increment;
 
@@ -46,7 +46,7 @@ SELECT * FROM CashPerPlayerPerGame;
 
 -- ADDED TO SCHEMA.SQL
 -- DELIMITER
--- prevents buying start, jail etc. common and shared cells
+-- prevents buying start (CellId1), jail etc. common and shared cells
 DELIMITER $$
 
 CREATE TRIGGER unbyuable_cells 
@@ -60,8 +60,23 @@ END$$
 
 DELIMITER ;
 
+-- same for update
+DELIMITER $$
 
--- TRANSACTION
+CREATE TRIGGER unbyuable_cells 
+  BEFORE UPDATE ON Player_has_Cell
+  FOR EACH ROW 
+BEGIN
+  IF (NEW.CellId IN (1,3,7,10,16,19,21,28,31,34)) THEN
+    CALL `The cell is unbuyable`;
+  END IF;
+END$$
+
+DELIMITER ;
+
+
+
+-- TRANSACTION CLAUSES ONLY FOR REFERENCE
 -- view autocommit value
 SELECT @@autocommit;
 
