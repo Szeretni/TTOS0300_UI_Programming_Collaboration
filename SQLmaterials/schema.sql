d98f43e0c1ee461ed82c7d330f4357ee4314f4ac
Szeretni <35224428+Szeretni@users.noreply.github.com>
2018-04-11 10:48:02 +0300

    added view, delimiter and isolation to eof

--- SQLmaterials/schema.sql
+++ SQLmaterials/schema.sql
@@ -249,3 +249,35 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+-- MANUAL CLAUSES, NOT FORWARD ENGINEERED
+-- creating the view
+CREATE VIEW CashPerPlayerPerGame
+AS
+SELECT GameSessionId,PlayerName,SUM(Value) as TotalCash 
+FROM Player
+INNER JOIN Player_has_Cash
+ON Player.PlayerId = Player_has_Cash.PlayerId
+INNER JOIN Cash
+ON Player_has_Cash.CashId = Cash.CashId
+GROUP BY GameSessionId,PlayerName
+;
+
+-- DELIMITER
+-- prevents buying start, jail etc. common and shared cells
+DELIMITER $$
+
+CREATE TRIGGER unbyuable_cells 
+  BEFORE INSERT ON Player_has_Cell
+  FOR EACH ROW 
+BEGIN
+  IF (NEW.CellId IN (1,3,7,10,16,19,21,28,31,34)) THEN
+    CALL `The cell is unbuyable`;
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- isolation level to serializable
+SET tx_isolation = 'SERIALIZABLE';
