e454c82a555f2960c4dbbc77da0e6993a38381f2
Szeretni <35224428+Szeretni@users.noreply.github.com>
2018-04-23 20:03:31 +0300

    card table added

--- SQLmaterials/20180423_ddl.sql
+++ SQLmaterials/20180423_ddl.sql
@@ -11,3 +11,7 @@ alter table GameSession add column (CurrentPlayerId int);
 alter table GameSession_has_player add column PlayerCash int;
 update GameSession_has_player set PlayerCash = 1500 where PlayerId in (1,2,3);
 update GameSession_has_player set PlayerCash = 1000 where PlayerId in (1);
+
+-- card table
+create table Card (CardId int primary key auto_increment, Effect varchar(64));
+insert into Card (Name,Description) values ("Lose100","Your relative died and you have to spend 100$ to travel expenses."),("Lose200","You have mismanaged your taxation and have to pay 200$."),("Gain100","Stockmarkets have raised and you gain 100$."),("Gain200","You gain 200$ because of you are such a nice person.");
