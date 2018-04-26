96948216d3df3411468dc47f2a2ef6edd4d0f349
Szeretni <35224428+Szeretni@users.noreply.github.com>
2018-04-26 15:31:54 +0300

    updated sql ddl

--- SQLmaterials/20180423_ddl.sql
+++ SQLmaterials/20180423_ddl.sql
@@ -15,3 +15,6 @@ update GameSession_has_player set PlayerCash = 1000 where PlayerId in (1);
 -- card table
 create table Card (CardId int primary key auto_increment, Effect varchar(64));
 insert into Card (Name,Description) values ("Lose100","Your relative died and you have to spend 100$ to travel expenses."),("Lose200","You have mismanaged your taxation and have to pay 200$."),("Gain100","Stockmarkets have raised and you gain 100$."),("Gain200","You gain 200$ because of you are such a nice person.");
+
+-- RentPaid field to GameSession_has_player
+alter table GameSession_has_player add column RentPaid bool default false;
